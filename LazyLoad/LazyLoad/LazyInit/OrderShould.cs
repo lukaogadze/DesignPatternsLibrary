ed51180d4df7570c1a156454cd65f3a790c7f792
loghadze <lukaogadze@live.com>
2018-10-12 16:33:20 +0400

    added LazyLoad

---
+++ LazyLoad/LazyLoad/LazyInit/OrderShould.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LazyLoad.LazyInit
+{
+    [TestFixture]
+    public class OrderShould
+    {
+        [Test]
+        public void PrintLabelWithGoodOrder()
+        {
+            var order = new OrderGood();
+
+            var result = order.PrintLabel();
+
+            Assert.AreEqual("Company Name\nDefault Address", result);
+        }
+
+        [Test]
+        public void PrintLabelWithBadOrder()
+        {
+            var order = new OrderBad();
+
+            // var result = order.PrintLabel();
+
+            // Assert.AreEqual("Company Name\nDefault Address", result);
+
+            Assert.Throws<NullReferenceException>(() => order.PrintLabel());
+        }
+
+        [Test]
+        public void PrintLabelWithLazyOrder()
+        {
+            var order = new OrderLazy();
+
+            var result = order.PrintLabel();
+
+            Assert.AreEqual("Company Name\nDefault Address", result);
+        }
+    }
+}
