6d078704f64e812fa1fa3ed684f02913f1e7851e
loghadze <lukaogadze@live.com>
2018-10-13 20:57:06 +0400

    added ObserverPattern

---
+++ ObserverPattern/Observer/EventAndDelegate/StockTicker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Observer.EventAndDelegate
+{
+	class StockTicker
+	{
+		private Stock stock;
+		public Stock Stock
+		{
+			get { return stock; }
+			set
+			{
+				stock = value;
+                OnStockChange(new StockChangeEventArgs(this.stock));
+			}
+		}
+
+		public event EventHandler<StockChangeEventArgs> StockChange;
+
+		protected virtual void OnStockChange(StockChangeEventArgs e)
+		{
+            StockChange?.Invoke(this, e);
+        }
+	}
+}
