a009b4906361b60f646ecd5a178e758754aa0f57
loghadze <lukaogadze@live.com>
2018-10-13 19:35:00 +0400

    added NullObjectPattern

---
+++ NullObjectPattern/Automobiles_1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Automobiles
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var autoRepository = new AutoRepository();
+            
+
+//            var mini = autoRepository.Find(new Guid("5E791286-902A-477A-86A4-B1424CCCB6B7"));
+//            if(mini == null)
+//                return ;
+//
+//            mini.Start();
+//            mini.Stop();
+
+
+            var automobiles = autoRepository.FindAllByName("ford");
+            foreach (var automobile in automobiles)
+            {
+                automobile.Start();
+                automobile.Stop();
+            }
+        }
+    }
+}
---
+++ NullObjectPattern/Automobiles_2/Autos/AutomobileBase.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Automobiles.Autos
+{
+    public abstract class AutomobileBase
+    {
+        public abstract Guid Id { get; }
+        public abstract string Name { get; }
+        public abstract void Start();
+        public abstract void Stop();
+
+        #region NULL
+
+        private static readonly NullAutomobile _nullAutomobile = new NullAutomobile();
+
+        public static NullAutomobile NULL => _nullAutomobile;
+
+        public class NullAutomobile : AutomobileBase
+        {
+            public override Guid Id
+            {
+                get { return Guid.Empty; }
+            }
+
+            public override string Name
+            {
+                get { return string.Empty; }
+            }
+
+            public override void Start() { }
+            public override void Stop() { }
+        }
+        
+        #endregion
+    }
+}
